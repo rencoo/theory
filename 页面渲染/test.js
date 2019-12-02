7913383fe52336e06a1efbf505bd50e4994041f2
rencoo <rencoogle@outlook.com>
2019-12-02 15:29:36 +0800

    页面渲染原理

--- 页面渲染/test.js
+++ 页面渲染/test.js
@@ -1,3 +1,9 @@
-for(var i=0; i<50000; i++) {
-    console.log(i);
+// 这段脚本至少执行 5s
+function longCode() {
+    var tStart = Number(new Date());
+    while((tStart + 5000) > Number(new Date())) {
+        console.log("继续执行");
+    }
 }
+
+longCode();
