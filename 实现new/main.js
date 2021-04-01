d83eb80f60dcbc2f37a5f173905c9be405c5d67d
rencoo <rencoogle@outlook.com>
2021-04-01 20:57:38 +0800

    improvement(new): improve new

--- 实现new/main.js
+++ 实现new/main.js
@@ -48,3 +48,20 @@ function newFake() {
     // 返回类型判断, 如果是对象则返回构造函数返回的对象；否则返回创建的新对象
     return typeof result === 'object' ? result : obj;
 };
+
+// https://javascript.ruanyifeng.com/oop/basic.html
+function _new(/* 构造函数 */ constructor, /* 构造函数参数 */ params) {
+  // 将 arguments 对象转为数组
+  var args = [].slice.call(arguments);
+  // 取出构造函数
+  var constructor = args.shift();
+  // 创建一个空对象，继承构造函数的 prototype 属性
+  var context = Object.create(constructor.prototype);
+  // 执行构造函数
+  var result = constructor.apply(context, args);
+  // 如果返回结果是对象，就直接返回，否则返回 context 对象
+  return (typeof result === 'object' && result != null) ? result : context;
+}
+
+// 实例
+var actor = _new(Person, '张三', 28);
